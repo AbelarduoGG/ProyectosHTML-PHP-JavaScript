0c19de14ebbde1d433cf25575df93f146d7b969a
Abel Vargas <aivargasrodriguez@gmail.com>
2024-04-20 17:27:07 -0600

    a

--- InnovateStyle/InnovateStylebk/models/sensor2.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');       //traemos la paquteria de mongoose
-
-const Sensor2Schema = new mongoose.Schema({  //creamos nuestro objeto json
-    
-    nSensor:String,
-    estado:String,
-}); 
-
-mongoose.model("Sensor2", Sensor2Schema);
