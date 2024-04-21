95ee97714ef0af030c729aacd5953ff99aadef4b
Abel Vargas <aivargasrodriguez@gmail.com>
2024-04-20 22:41:20 -0600

    hg

--- InnovateStyle/InnovateStylebk/models/arduino.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');       //traemos la paquteria de mongoose
-
-const ArduinoSchema = new mongoose.Schema({  //creamos nuestro objeto json
-    
-    nSensor:String,
-    valor:String,
-}); 
-
-mongoose.model("Arduino", ArduinoSchema);
